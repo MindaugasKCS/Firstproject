3356afe13be5632c2cc830062f492452884f7d70
MindaugasKCS <mindaugas.cernauskas@gmail.com>
2018-05-14 19:41:40 +0300

    init project

---
+++ src/FisrtDay/FisrtTask.java
@@ -0,0 +1,31 @@
+package FisrtDay;
+
+import java.util.Scanner;
+
+public class FisrtTask {
+
+    public static void main(String[]args){
+
+        Scanner scanner = new Scanner(System.in);
+        String word;
+        do {
+            System.out.println("Iveskite zodi");
+             word = scanner.nextLine();
+             if (word.length() % 2 == 0){
+                 System.out.println("Ivestas zodis " + word + " yra lyginis, rasta raidziu " +cuontLetters(word, 'a'));
+             }
+             else{
+                 System.out.println("Ivestas zodis " + word +" yra nelyginis, rasta raidziu " +cuontLetters(word, 'a'));
+             }
+        } while(!word.equals("pabaiga"));
+    }
+    private static int cuontLetters(String word, char letter){
+        int cuont =0;
+        for (int i=0; i < word.length(); i++){
+            if (word.charAt(i) ==letter){
+                cuont ++;
+            }
+        }
+        return cuont;
+    }
+}
---
+++ src/MyFirstClass.java
@@ -0,0 +1,51 @@
+import java.sql.SQLOutput;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class MyFirstClass {
+    public static void main(String[] args){
+        MyFirstClass myFirstClass = new MyFirstClass();{
+
+            float ff = 5.6f;
+
+            myFirstClass.notSatticmethod("Value");
+            System.out.println(sayHi());
+            myFirstClass.checThisNumber(50);
+            System.out.println("Please insert a number");
+            Scanner scaner = new Scanner(System.in);
+
+            try {
+                int numb = scaner.nextInt();
+                System.out.println("Yuor number is -> " + numb);
+            }catch (InputMismatchException e){
+                System.out.println("Ivedete ne skaiciu");
+            }
+
+        }
+    }
+
+    private void notSatticmethod(String name){
+        System.out.println(name);
+        sayHi();
+
+    }
+
+    private static String sayHi(){
+
+
+        return " Hi Andrius";
+    }
+
+    private void checThisNumber(int numb){
+        if(numb >10 && numb < 60){
+            System.out.println("Daugiau");
+        }
+         else if((numb >15)){
+            System.out.println("Daugiau uz 15");
+        }
+        else{
+            System.out.println("maziau");
+        }
+    }
+
+}
